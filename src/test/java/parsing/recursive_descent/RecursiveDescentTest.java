8218b67bea07cf830b8229f58dcd9f617e99faae
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-02-21 21:01:09 +0100

    Improve test

--- src/test/java/parsing/recursive_descent/RecursiveDescentTest.java
+++ src/test/java/parsing/recursive_descent/RecursiveDescentTest.java
@@ -9,14 +9,21 @@ import org.junit.Test;
 public class RecursiveDescentTest {
 
 	@Test
-	public void test() {
+	public void testOk() {
 		
 		ListLexer lexer = new ListLexer("[a,b]");
 		ListParser parser = new ListParser(lexer);
-		
+
 		// no throw
+		parser.list();		
+	}
+	
+	@Test(expected=java.lang.Error.class)
+	public void testFail() {
+		ListLexer lexer = new ListLexer("[a,]");
+		ListParser parser = new ListParser(lexer);
+
 		parser.list();
-		
 	}
 
 }
